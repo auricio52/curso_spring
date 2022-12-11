8baae2b4073696326fe7b71ecefe7e3ea8cb342b
auricio52 <auricio52@gmail.com>
2022-12-11 19:32:19 -0300

    User delete

--- src/main/java/com/empresa/course/controllers/UserController.java
+++ src/main/java/com/empresa/course/controllers/UserController.java
@@ -37,4 +37,10 @@ public class UserController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
